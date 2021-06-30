ce62293af1a3a1bcf563c87a9e763c580d160c54
unknown <aamacanitan@gmail.com>
2021-06-30 16:26:30 -0400

    No aparece formumlario

--- DjangoApp/anubis/forms.py
+++ DjangoApp/anubis/forms.py
@@ -2,3 +2,86 @@ from django import forms
 from django.forms import ModelForm, widgets
 from django.forms.models import ModelChoiceField
 from django.forms.widgets import Widget
+from .models import Colaboradores
+
+
+class ColaboradoresForm(ModelForm):
+
+    class Meta:
+        Model = Colaboradores
+        fields = ['rut','image','nombreComp','telefono','direccion','email','contrasenia']
+
+    widgets={
+        'rut': forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'id': 'rut',
+                'name': 'rut',
+                'type': 'text',
+                'placeholder': 'Ingrese su Rut'
+            }
+        ),
+        'nombreComp': forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'id': 'nombre',
+                'name': 'nombre',
+                'type': 'text',
+                'placeholder': 'Nombre completo:'
+            }
+        ),
+        'image': forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'id': 'foto',
+                'name': 'foto',
+                'type': 'file',
+                'placeholder': 'Adjuntar foto'
+            }
+        ),
+        'email': forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'id': 'email',
+                'name': 'email',
+                'type': 'text',
+                'placeholder': 'Email: '
+            }
+        ),
+        'telefono': forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'id': 'fono',
+                'name': 'fono',
+                'type': 'number',
+                'placeholder': 'Telefono: '
+            }
+        ),
+        'contrasenia': forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'id': 'contra',
+                'name': 'contra',
+                'type': 'password',
+                'placeholder': 'Contraseña: '
+            }
+        ),
+        'direccion': forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'id': 'direccion',
+                'name': 'direccion',
+                'type': 'text',
+                'placeholder': 'Ingrese su Direccion'
+            }
+        ),
+        'pais': forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'id': 'rut',
+                'name': 'rut',
+                'type': 'text',
+                'placeholder': 'Ingrese su Rut'
+            }
+        )
+    }
--- DjangoApp/anubis/views.py
+++ DjangoApp/anubis/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render, redirect
-from .models import Colaboradores
+from .forms import ColaboradoresForm
 # Create your views here.
 
 def formulario(request):
     return render(request, 'formulario.html')
+
+def form_colaboradores(request):
+    if request.method =='POST':
+        colab=ColaboradoresForm(request.POST)
+        if colab.is_valid():
+            colab.save()
+            return redirect('formulario')
+    else:
+        colab=ColaboradoresForm()
+    return render(request, 'anubis/formulario.html', {'colab': colab})
